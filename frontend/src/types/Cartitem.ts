ee50e0741cd763eac16e4efc74209e45a9ae9b48
Rusell <russellepling@gmail.com>
2025-04-03 17:50:24 -0600

    Added CRUD Functionality

--- frontend/src/types/Cartitem.ts
+++
@@ -1,7 +0,0 @@
-export interface CartItem {
-  bookId: number;
-  title: string;
-  price: number;
-  quantity: number;
-  image?: string; // Optional: If you want to display product images
-}
--- frontend/src/types/Projects.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-  projectId: number;
-  projectName: string;
-  ProjectType: string;
-  ProjectRegionalProgram: string;
-  ProjectImpact: number;
-  projectPhase: string;
-  projectFunctionalilyStatus: string;
-}
